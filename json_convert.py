322ab53ec5cc8667a1bf263cf18f81a9beed28c6
athros <agent6170@gmail.com>
2016-03-04 06:32:43 -0700

    update to the script, prepping for actual formatting
    Initial unit strip is just about done. Example is also being committed.

--- json_convert.py
+++ json_convert.py
@@ -1,5 +1,19 @@
 import json
 
+def explode_list(key, s_dict):
+    print('\n-----%s start-----' % key)   
+    for k, v in s_dict.items():
+        if isinstance(v, dict):
+            #print(k + ' : ' + str(v))
+            explode_list(k, v)
+        elif isinstance(v, list):
+            print('--%s start--' % k)
+            print(k + ' : ' + str(v))
+            print('--%s end--' % k)
+        else:
+            print(k + ' : ' + str(v))
+    print('-----%s end-----\n' % key)
+
 f = open('units.json', 'r')
 jsondata = json.load(f)
 
@@ -9,13 +23,12 @@ walldict = d1['wallsTemplates']
 del d1['unitsTemplates']
 del d1['wallsTemplates']
 
-print(d1)
 for item in unitdict['WorldUnitTemplate']:
     if item['race'] == 'HIGH_MEN':
         print('\n------START-----\n')
         for key in item:
-            print(key + ' : ' + str(item[key]) + '\n')
+            if isinstance(item[key], (list, dict)):
+                explode_list(key, item[key])
+            else:
+                print(key + ' : ' + str(item[key]) + '\n')
         print('\n-----END-----')
-#print(unitdict)
-print('\n\n\n')
-#print(walldict)
