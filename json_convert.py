cdb553986daa469e73b982a4c74ce32238b71f46
athros <agent6170@gmail.com>
2016-03-04 08:06:07 -0700

    Loading up the last 2 files and adding comments

--- json_convert.py
+++ json_convert.py
@@ -1,6 +1,7 @@
 import json
 
 def explode_list(key, s_dict):
+    ''' Run through a list piece by piece '''
     print('\n-----%s start-----' % key)   
     for k, v in s_dict.items():
         if isinstance(v, dict):
@@ -14,14 +15,30 @@ def explode_list(key, s_dict):
             print(k + ' : ' + str(v))
     print('-----%s end-----\n' % key)
 
-f = open('units.json', 'r')
-jsondata = json.load(f)
+# There are 3 files total with information in them.
+# Load the file, convert to python data structures
+# Close the file. 
 
-d1 = jsondata['WorldUnitTemplatesContainer']
-unitdict = d1['unitsTemplates']
-walldict = d1['wallsTemplates']
-del d1['unitsTemplates']
-del d1['wallsTemplates']
+unit_file = open('units.json', 'r')
+unitdata = json.load(unit_file)
+unit_file.close()
+
+spell_ability_file = open('spellsAndAbilities.json', 'r')
+sadata = json.load(spell_ability_file)
+spell_ability_file.close()
+
+dlc_file = open('dlc.json', 'r')
+dlcdata = json.load(dlc_file)
+dlc_file.close()
+
+
+# Begin parsing out the dicts and nested structures to find the information we need.
+# This is super basic at the moment, no formatting since I'm trying to figure out 
+# how to pull and format properly in flat text.
+
+full_unitdict = unitdata['WorldUnitTemplatesContainer']
+unitdict = full_unitdict['unitsTemplates']
+walldict = full_unitdict['wallsTemplates']
 
 for item in unitdict['WorldUnitTemplate']:
     if item['race'] == 'HIGH_MEN':
