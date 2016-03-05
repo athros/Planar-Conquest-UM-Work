9451af82fe5a4bb2f3738786035696adc923fb69
athros <agent6170@gmail.com>
2016-03-05 16:51:54 -0700

    Updates to tactical spells - now working

--- json_convert.py
+++ json_convert.py
@@ -9,6 +9,15 @@ def explode_list(key, s_dict):
         if isinstance(v, dict):
             #print(k + ' : ' + str(v))
             explode_list(k, v)
+        # Tactical spells are under tacticalSpells/int 
+        elif k == 'int':
+            i = 0
+            for spe in v:
+                if spe != '0':
+                    print(i, get_spell(spe))
+                    i = i + 1
+                else:
+                    print('0 No spells')
         elif isinstance(v, list):
             print('--%s start--' % k)
             print(k + ' : ' + str(v))
@@ -16,6 +25,18 @@ def explode_list(key, s_dict):
         else:
             print(k + ' : ' + str(v))
     print('-----%s end-----\n' % key)
+    
+def get_spell(id_num):
+    '''Take in an id, output spell name'''
+    spell = ''
+    for sp in tactical_spells:
+        if sp['id'] == str(id_num):
+            spell = sp['spellName']
+    return spell
+
+################
+# Script Start #
+################
 
 # There are 3 files total with information in them.
 # Load the file, convert to python data structures
@@ -33,29 +54,40 @@ dlc_file = open('dlc.json', 'r')
 dlcdata = json.load(dlc_file)
 dlc_file.close()
 
+# Begin parsing out the dicts and nested structures into seperate 
+# dicts for easier parsing.
 
-# Begin parsing out the dicts and nested structures to find the information we need.
-# This is super basic at the moment, no formatting since I'm trying to figure out 
-# how to pull and format properly in flat text.
-
+# Units
 full_unitdict = unitdata['WorldUnitTemplatesContainer']
 unitdict = full_unitdict['unitsTemplates']
 walldict = full_unitdict['wallsTemplates']
 
-'''for item in unitdict['WorldUnitTemplate']:
-    if item['race'] == 'HIGH_MEN' and item['battleStats']['name'] == 'Clerics':
-        print('\n------START-----\n')
+#Spells and Abilities
+full_sa_dict = sadata['globalSpellbook']
+strategic_abilities = full_sa_dict['strategicAbilities']
+tactical_abilities = full_sa_dict['tacticalAbilities']
+strategic_spells = full_sa_dict['strategicSpells']
+tactical_spells = full_sa_dict['tacticalSpells']['tacticalSpell']
+auras = full_sa_dict['auras']
+
+
+
+# Parsers start here. 
+for item in unitdict['WorldUnitTemplate']:
+    print(item['race'])
+    '''if item['race'] == 'HIGH_MEN' and (item['battleStats']['type'] == 'HERO' or item['battleStats']['type'] == 'CHAMPION'): #and item['battleStats']['name'] == 'Clerics':
+        print('\n------%s START-----\n' % (str(item['battleStats']['name'])))
         for key in item:
             if isinstance(item[key], (list, dict)):
                 explode_list(key, item[key])
             else:
                 print(key + ' : ' + str(item[key]) + '\n')
-        print('\n-----END-----')'''
+        print('\n-----%s END-----' % (str(item['battleStats']['name'])))'''
 
-for item in unitdict['WorldUnitTemplate']:
+'''for item in unitdict['WorldUnitTemplate']:
     print('Unit name: %s' % item['battleStats']['name'])
     print('Unit Race: %s' % item['race'])
     print('\nUnit Movement Stats:\tType of movement: %s\n\t\t\tMovement Points: %s\n\t\t\tRoad Modifier: %s\n' %
             (item['movement']['type'], str(item['movement']['points']), str(item['movement']['roadMultiplier'])))
     print('Upkeep Costs:\tGold: %s\n\t\tMana: %s\n\t\tFood Upkeep: %s\n\t\tNegative Energy: %s\n' %
-          (str(item['goldUpkeepCost']), str(item['manaUpkeepCost']), str(item['foodUpkeepCost']), str(item['neUpkeepCost'])))
+          (str(item['goldUpkeepCost']), str(item['manaUpkeepCost']), str(item['foodUpkeepCost']), str(item['neUpkeepCost'])))'''
