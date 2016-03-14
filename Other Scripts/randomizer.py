5c0a345b5c1ce187001eaf219ee8f21c921c69d6
athros <agent6170@gmail.com>
2016-03-14 09:32:04 -0600

    Updating the randomizer

--- Other Scripts/randomizer.py
+++ Other Scripts/randomizer.py
@@ -1,12 +1,33 @@
 import random
 
-PLANES = ['Prime', 'Water', 'Fire', 'Earth', 'Air', 'Paradise', 'Shadow']
+RACES = ['High Men',
+         'Unhallowed',
+         'Gray Elves',
+         'Dark Elves',
+         'Dwarves',
+         'Myrodants',
+         'Orcs',
+         'Draconians']
 
-TYPE_OF_LANDMASS = ['Pangea', 'Two Contenints', 'Three Continents', 'Islands']
+PLANES = ['Prime',
+          'Water',
+          'Fire',
+          'Earth',
+          'Air',
+          'Paradise',
+          'Shadow']
+
+TYPE_OF_LANDMASS = ['Pangea',
+                    'Two Contenints',
+                    'Three Continents',
+                    'Islands']
+
 LANDMASS_PERCENTAGE = ['30%', '50%', '70%', '90%']
 
 NUM_OF_PLANES = random.randint(1, 5)
 PLANES_TO_PLAY = {}
+
+RACE_TO_PLAY = RACES[random.randint(1, len(RACES)) - 1]
 
 for i in range(1, NUM_OF_PLANES):
     plane = PLANES[random.randint(1, len(PLANES)) - 1]
@@ -14,5 +35,8 @@ for i in range(1, NUM_OF_PLANES):
     landmass_type = TYPE_OF_LANDMASS[random.randint(1, len(TYPE_OF_LANDMASS)) - 1]
     landmass_percent = LANDMASS_PERCENTAGE[random.randint(1, len(LANDMASS_PERCENTAGE)) - 1]
     PLANES_TO_PLAY[plane] = "%s at %s" % (landmass_type, landmass_percent)
-    
-print(PLANES_TO_PLAY)
+
+print('Your Race is %s' % RACE_TO_PLAY)
+print('The chosen planes are: ')
+for pl in PLANES_TO_PLAY:
+    print('\t%s with the options %s' % (pl, PLANES_TO_PLAY[pl]))
